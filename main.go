a214a59c21c28bbd6159803c4f6b4592371afc2e
Pyakz <MarkVergelBanguis@gmail.com>
2021-04-20 11:02:25 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+
+firstName := "Mark Vergel"
+lastName := "Banguis"
+
+age := 21
+address := "Surigao City"
+school := "Surigao State College of Technology"
+company := "Cauld and Clark"
+
+
+fmt.Println("Fullname: " + firstName + " " +lastName)
+
+fmt.Println("Works at: " + company)
+
+fmt.Printf("%s %d\n", "Age:" , age)
+
+fmt.Println("Current Adress: " + address)
+
+fmt.Println("University: " + school)
+
+
+}
