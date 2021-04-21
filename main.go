0ab5d90f3a2d06ea39b4230328509355847246ee
Pyakz <MarkVergelBanguis@gmail.com>
2021-04-21 13:54:48 +0800

    ..

--- main.go
+++ main.go
@@ -1,35 +1,92 @@
 package main
 
 import (
-	"fmt"
+    "bufio"
+    "fmt"
+    "os"
+	"strconv"
 )
 
 func main() {
 
-	// var (Variable Name) (Variable Type) = (Variable Assignment)
-	var firstName string = "Mark Vergel"
 
-	// Shortcut Declaration
-	lastName := "Banguis"
-	age := 21
-	address := "Surigao City"
-	school := "Surigao State College of Technology"
-	company := "Cauld and Clark"
-	weight := 54.50
-	height := 168
-
-	fmt.Println("\nFullname: " + firstName + " " + lastName)
-
-	fmt.Println("Works at: " + company)
-
-	fmt.Printf("%s %d\n", "Age:", age)
-
-	fmt.Printf("%s %d\n", "Height:", height)
-
-	fmt.Printf("%s %f\n", "Weight:", weight)
-
-	fmt.Println("Current Adress: " + address)
-
-	fmt.Println("University: " + school)
 
 }
+
+
+
+
+
+
+func Shop() {
+
+	fmt.Println("::::::::::::::::::::::::::::::::::::::")
+	fmt.Println("::  [1]    ---------------    BUY   ::")
+	fmt.Println("::  [2]    ---------------    View  ::")
+	fmt.Println("::  [3]    ---------------    Exit  ::")
+	fmt.Println("::::::::::::::::::::::::::::::::::::::")
+
+	// Create a new scanner object
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Printf("Choose: ")
+	scanner.Scan()
+
+	input, _ := strconv.ParseInt(scanner.Text(), 10, 64 )
+
+	if(input == 1) {
+		fmt.Println("BUY")
+	} else if (input == 2) {
+		fmt.Println("View")
+	} else if (input == 3) {
+		fmt.Println("Exit")
+	} else {
+		fmt.Println("Please Choose the right command")
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// // var (Variable Name) (Variable Type) = (Variable Assignment)
+	// var firstName string = "Mark Vergel"
+
+	// // Pwede
+	// var age = 21
+	// var height = 168
+
+	// // Shortcut Declaration
+	// lastName 	:= "Banguis"
+	// address 	:= "Surigao City"
+	// school 		:= "Surigao State College of Technology"
+	// company 	:= "Cauld and Clark"
+	// weight 		:= 54.305345345
+
+	// fmt.Println("\nFullname: " + firstName + " " + lastName)
+
+	// fmt.Println("Works at: " + company)
+
+	// fmt.Printf("%s %d\n", "Age:", age)
+
+	// fmt.Printf("%s %d\n", "Height:", height)
+
+	// fmt.Printf("%s %.2f\n", "Weight:", math.Floor(weight*100)/100)
+
+	// fmt.Println("Current Adress: " + address)
+
+	// fmt.Println("University: " + school)
